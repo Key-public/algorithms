4e6591801bbddd98534290a4f04953c99b74f1c6
Key-pc-win <svoronov@bk.ru>
2020-06-10 18:11:29 +0300

    done

--- binarySearh.py
+++ binarySearh.py
@@ -5,17 +5,26 @@ TODO:
 3. В тело функции добавить алгоритм
 4. На выходе функция должна возвращать загаданное число (если возможно, иначе возвращать сообщение о том, что число отсутствует в массиве) и количество шагов
 """
-sorted_list = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23} # простые числа по возрастанию
-the_number = 3
-counter = 0
-def binarySearch(sorted_list, the_number):
-    while check != the_number:
-        counter += 1
-        check = len(sorted_list) / 2
-        if check = 0:
-            print("There is no the_number in the array. I found this in", counter,  "tries")
-        if sorted_list(check) < the_number:
-            sorted_list = sorted_list[:check-1]
+
+
+sorted_list = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23] # простые числа по возрастанию
+the_number = 5
+
+def binarySearch(array, guess):
+    assert type(guess) == int, 'the_number must be integer'
+    counter = 1
+    check = len(array) // 2
+    while array[check] != guess:
+        if check == 0:
+            print("There is no", the_number, "in the array. I found this with", counter,  "tries")
+            return 1
+        if array[check] > guess:
+            array = array[:check]
         else:
-            sorted_list = sorted_list[check+1:]
-    print("the_number is", check, ". I found this in", conuter, "tries")
+            array = array[check+1:]
+        check = len(array) // 2
+        counter += 1
+    print("It's", array[check], "and I found this with", counter, "tries")
+    return 0
+
+binarySearch(sorted_list, the_number)
